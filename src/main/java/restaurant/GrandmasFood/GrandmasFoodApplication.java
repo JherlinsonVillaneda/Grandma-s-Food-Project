1ab11fce81e94d527b923878ef7eed8bc70cdb12
Jherlinson Stiven Villaneda Carvajal <jherlinson.villaneda@globant.com>
2024-02-23 09:00:21 -0500

    First Commit

---
+++ src/main/java/restaurant/GrandmasFood/GrandmasFoodApplication.java
@@ -0,0 +1,13 @@
+package restaurant.GrandmasFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandmasFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandmasFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restaurant/GrandmasFood/GrandmasFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurant.GrandmasFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandmasFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
