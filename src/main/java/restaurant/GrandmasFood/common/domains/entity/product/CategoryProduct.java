481758da0d26baae6811d8db7dd86ae66643a707
Andres Felipe Yate Munoz <andres.yate@globant.com>
2024-02-26 20:45:10 -0500

    Add entities and structure schema

---
+++ src/main/java/restaurant/GrandmasFood/common/domains/entity/product/CategoryProduct.java
@@ -0,0 +1,11 @@
+package restaurant.GrandmasFood.common.domains.entity.product;
+
+public enum CategoryProduct {
+    HAMBURGERS_AND_HOTDOGS,
+    CHICKEN,
+    FISH,
+    MEATS,
+    DESSERTS,
+    VEGAN_FOOD,
+    KIDS_MEALS
+}
--- src/main/java/restaurant/GrandmasFood/repositories/ClientRepository/IClientRepository.java
+++ src/main/java/restaurant/GrandmasFood/repositories/ClientRepository/IClientRepository.java
@@ -1,4 +1,16 @@
 package restaurant.GrandmasFood.repositories.ClientRepository;
 
-public interface IClientRepository {
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import restaurant.GrandmasFood.common.domains.entity.client.ClientEntity;
+
+public interface IClientRepository extends JpaRepository<ClientEntity, String> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM client c WHERE c.document = :document_param")
+    void deleteByDocument(@Param("document_param") String document_param);
 }
